5af8392687ce2a9c412237bc3f8e59314ae9ff47
BALOCKAY909 <123591627+BALOCKAYY@users.noreply.github.com>
2026-04-02 19:27:19 -0500

    Remove customer reviews section, See All Reviews button, and all-reviews URL from site

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.quote_request, name='quote_request'),
     path('thank-you/', views.thank_you, name='thank_you'),
-    path('all-reviews/', views.all_reviews, name='all_reviews'),
     path('services/', views.services, name='services'),
     path('areas-served/', views.areas_served, name='areas_served'),
     path('verifyreviews/', views.review_login, name='review_login'),
