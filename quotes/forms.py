5299ae8ae8d729dad27d25dc591c73289980521d
BALOCKAY909 <123591627+BALOCKAYY@users.noreply.github.com>
2026-05-16 13:57:34 -0500

    Allow .net email addresses in addition to .com

--- quotes/forms.py
+++ quotes/forms.py
@@ -4,7 +4,7 @@ import re
 class QuoteRequestForm(forms.Form):
     first_name = forms.CharField(max_length=30, label='First Name', widget=forms.TextInput(attrs={'required': 'required'}))
     last_name = forms.CharField(max_length=30, label='Last Name', widget=forms.TextInput(attrs={'required': 'required'}))
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'required': 'required', 'pattern': r'.+@.+\.com$'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'required': 'required', 'pattern': r'.+@.+\.(com|net)$'}))
     phone = forms.CharField(max_length=20, label='Phone Number', widget=forms.TextInput(attrs={'required': 'required', 'pattern': r'^(\+1\s?)?(\(?\d{3}\)?[\s-]?)?\d{3}[\s-]?\d{4}$'}))
     job_description = forms.CharField(widget=forms.Textarea(attrs={'required': 'required'}), label='Job Description')
 
@@ -18,6 +18,6 @@ class QuoteRequestForm(forms.Form):
 
     def clean_email(self):
         email = self.cleaned_data['email']
-        if not email.endswith('.com'):
-            raise forms.ValidationError('Email must end with .com')
+        if not (email.endswith('.com') or email.endswith('.net')):
+            raise forms.ValidationError('Email must end with .com or .net')
         return email
